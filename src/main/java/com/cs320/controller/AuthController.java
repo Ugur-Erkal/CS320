f03693bfdedc633a5efdadb488894091524b682e
Atakan Karatas <atakan.karatas@ozu.edu.tr>
2025-12-20 15:32:28 +0300

    refactor: remove unused imports and add basic null-safety

--- src/main/java/com/cs320/controller/AuthController.java
+++ src/main/java/com/cs320/controller/AuthController.java
@@ -2,15 +2,12 @@ package com.cs320.controller;
 
 import com.cs320.service.UserService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Map;
 
 @Controller
 public class AuthController {
