b0c56907c1897c64855d0474f334f4a294d722e7
kayragoksin <kayra.goksin@ozu.edu.tr>
2025-12-21 20:47:54 +0300

    datasource and user class added

--- src/main/java/models/user.java
+++ src/main/java/models/user.java
@@ -1,21 +1,38 @@
 package models;
 
-public class user {
-    private final int id;
-    private final String username;
-    private final String passwordHash;
-    private final String role;    // MEMBER, TRAINER или ADMIN
+public class User {
 
-    public user(int id, String username, String passwordHash, String role) {
-        this.id = id;
+    private int userId;
+    private String username;
+    private String password;
+    private String userType; // "Customer" veya "Manager"
+
+    public User(int userId, String username, String password, String userType) {
+        this.userId = userId;
         this.username = username;
-        this.passwordHash = passwordHash;
-        this.role = role;
+        this.password = password;
+        this.userType = userType;
     }
-    public int getId() { return id; }
-    public String getUsername() { return username; }
-    public String getPasswordHash() { return passwordHash; }
-    public String getRole() { return role; }
+
+    public User(String username, String password, String userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
 }
-
-
