acd367cb8bf13ad3e9eeb529d61609c5d4a22732
kayragoksin <kayra.goksin@ozu.edu.tr>
2025-12-21 21:04:27 +0300

    payment class updated according to ddl

--- src/main/java/models/payment.java
+++ src/main/java/models/payment.java
@@ -1,43 +1,44 @@
 package models;
 
+
+
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
-public class payment {
-    public class Payment {
-        private final int id;
-        private final int memberId;
-        private final String username;
-        private final BigDecimal amount;
-        private final Timestamp paidAt;
+public class payment{
 
-        public Payment(int id, int memberId, String username, BigDecimal amount, Timestamp paidAt) {
-            this.id = id;
-            this.memberId = memberId;
-            this.username = username;
-            this.amount = amount;
-            this.paidAt = paidAt;
-        }
+    private int paymentId;
+    private int userId;
+    private BigDecimal amount;
+    private Timestamp paidAt;
 
-        public int getId() {
-            return id;
-        }
 
-        public int getMemberId() {
-            return memberId;
-        }
+    public payment(int paymentId, int userId, BigDecimal amount, Timestamp paidAt) {
+        this.paymentId = paymentId;
+        this.userId = userId;
+        this.amount = amount;
+        this.paidAt = paidAt;
+    }
 
-        public String getUsername() {
-            return username;
-        }
 
-        public BigDecimal getAmount() {
-            return amount;
-        }
+    public payment(int userId, BigDecimal amount) {
+        this.userId = userId;
+        this.amount = amount;
+    }
 
-        public Timestamp getPaidAt() {
-            return paidAt;
-        }
+    public int getPaymentId() {
+        return paymentId;
+    }
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Timestamp getPaidAt() {
+        return paidAt;
     }
 }
