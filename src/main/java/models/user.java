d8ef163542795a7891e63f8d9d9e1a347d27204e
kayragoksin <kayra.goksin@ozu.edu.tr>
2025-12-22 21:39:23 +0300

    user updated

--- src/main/java/models/user.java
+++ src/main/java/models/user.java
@@ -1,20 +1,20 @@
 package models;
 
-public class User {
+public class user {
 
     private int userId;
     private String username;
     private String password;
     private String userType; // "Customer" veya "Manager"
 
-    public User(int userId, String username, String password, String userType) {
+    public user(int userId, String username, String password, String userType) {
         this.userId = userId;
         this.username = username;
         this.password = password;
         this.userType = userType;
     }
 
-    public User(String username, String password, String userType) {
+    public user(String username, String password, String userType) {
         this.username = username;
         this.password = password;
         this.userType = userType;
